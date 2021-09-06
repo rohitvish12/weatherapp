c1e27b3da8374377ba79e311aaf52a9e81a301ff
Rohit Vishwakarma <69453375+rohitec01@users.noreply.github.com>
2021-09-06 21:34:30 +0530

    some changes

--- frontend/src/redux/constants/productConstants.js
+++
@@ -1,5 +0,0 @@
-export const GET_PRODUCT_SUCCESS = 'getProductsSuccess';
-export const GET_PRODUCT_FAIL = 'getProductsFail';
-
-export const GET_PRODUCT_DETAIL_SUCCESS = 'getProductsDetailsSuccess';
-export const GET_PRODUCT_DETAIL_FAIL = 'getProductsDeatilsFail';
